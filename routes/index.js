d6686feec10bc54f9e1688bd092c170a26bda176
Luigi Poole <luigipoole@outlook.com>
2015-12-05 16:33:37 -0800

    go away.

--- routes/index.js
+++ routes/index.js
@@ -27,7 +27,7 @@ router.post('/invite', function(req, res) {
         if (body.ok) {
           res.render('result', {
             community: config.community,
-            message: 'Success! Check "'+ req.body.email +'" for an invite from Slack.'
+            message: 'Success! Check your email.'
           });
         } else {
           var error = body.error;
